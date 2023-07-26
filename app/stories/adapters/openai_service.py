ddb2c765da710a0d0dba2e1ef7993462f3468983
naziya115 <naziyazhanabayeva@gmail.com>
2023-07-26 13:56:39 +0600

    updated number of tokens

--- app/stories/adapters/openai_service.py
+++ app/stories/adapters/openai_service.py
@@ -18,7 +18,7 @@ class OpenAIService:
             streaming=True,
             callbacks=[StreamingStdOutCallbackHandler()],
             temperature=0.5,
-            max_tokens=50,
+            max_tokens=500,
         )
 
         self.all_users = []
